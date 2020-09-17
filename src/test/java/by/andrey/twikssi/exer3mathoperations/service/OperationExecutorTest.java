1531dca6defb9936e22ef998982725e4f4ffcd02
twikssi <limpbiski15@gmail.com>
2020-09-17 16:24:51 +0300

    added mock properties and import mock

--- src/test/java/by/andrey/twikssi/exer3mathoperations/service/OperationExecutorTest.java
+++ src/test/java/by/andrey/twikssi/exer3mathoperations/service/OperationExecutorTest.java
@@ -2,10 +2,17 @@ package by.andrey.twikssi.exer3mathoperations.service;
 
 import by.andrey.twikssi.exer3mathoperations.interfaces.MathOperation;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OperationExecutorTest {
+
+    @Mock
+
 
     @Test
     public void execute() {
