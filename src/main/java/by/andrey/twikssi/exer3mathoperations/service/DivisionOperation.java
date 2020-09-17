57a87371ba9e54373eb4cee1636ab64bd089d8e7
twikssi <limpbiski15@gmail.com>
2020-09-17 16:31:17 +0300

    created override methods in class DivisionOperation and mock things

---
+++ src/main/java/by/andrey/twikssi/exer3mathoperations/service/DivisionOperation.java
@@ -0,0 +1,15 @@
+package by.andrey.twikssi.exer3mathoperations.service;
+
+import by.andrey.twikssi.exer3mathoperations.interfaces.MathOperation;
+
+public class DivisionOperation implements MathOperation {
+    @Override
+    public double compute(double a, double b) {
+        return a/b;
+    }
+
+    @Override
+    public String getNameClass() {
+        return "Division";
+    }
+}
--- src/test/java/by/andrey/twikssi/exer3mathoperations/service/OperationExecutorTest.java
+++ src/test/java/by/andrey/twikssi/exer3mathoperations/service/OperationExecutorTest.java
@@ -1,18 +1,29 @@
 package by.andrey.twikssi.exer3mathoperations.service;
 
 import by.andrey.twikssi.exer3mathoperations.interfaces.MathOperation;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OperationExecutorTest {
 
     @Mock
+    DivisionOperation divisionOperation;
 
+    @InjectMocks
+    OperationExecutor operationExecutor;
+
+    @Before
+    public void setUp(){
+        when(divisionOperation.)
+    }
 
     @Test
     public void execute() {
